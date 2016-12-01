ceaae06c4238aa3813df7cfef73ecff0d8c955f3
cristianeto <cristiang_guamanb@hotmail.com>
2016-12-01 15:46:33 -0500

    vuejs2 y firebase CRUD

--- vue.js
+++ vue.js
@@ -1,6 +1,22 @@
 /**
  * Created by Cristian on 30/11/16.
  */
+/*FIrebase*/
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyBhP1DZT7_hgfisz6zAGICp6Puh7FGESaE",
+    authDomain: "vuejs2todolist.firebaseapp.com",
+    databaseURL: "https://vuejs2todolist.firebaseio.com",
+    storageBucket: "vuejs2todolist.appspot.com",
+    messagingSenderId: "400410754704"
+};
+firebase.initializeApp(config);
+/*Fin fireBase*/
+
+/*Usando real Database*/
+// Get a reference to the database service
+var dataBase = firebase.database();
+
 Vue.component('todo-list',{
     template:'#todo-template',
     data:function(){
@@ -12,33 +28,51 @@ Vue.component('todo-list',{
     props:['tareas'],
     methods:{
         agregarTarea:function(tarea){
-            //console.info(tarea);
-            this.tareas.unshift({
-                titulo:tarea,
-                completado:false
+            dataBase.ref('tareas/').push({
+                titulo: tarea, completado: false,
             });
+            this.nuevaTarea='';
         },
         editarTarea:function(tarea){
-            console.info(tarea);
+            //console.info(tarea['.key']);
+            dataBase.ref('tareas/'+tarea['.key']).update({
+               titulo: tarea.titulo
+            });
         },
-        eliminarTarea: function(indice){
-            this.tareas.splice(indice,1); //eliminar el elemento de ese indice
+        actualizarEstadoTarea:function(estado, tarea){
+            dataBase.ref('tareas/'+tarea['.key']).update({
+                completado: estado?true:false,
+            });
+        },
+        eliminarTarea: function(tarea){
+            //this.tareas.splice(indice,1); //eliminar el elemento de ese indice
+            dataBase.ref('tareas/'+tarea['.key']).remove();
+
         }
+
     }
 });
 
-new Vue({
+var vm=new Vue({
     el: '#app',
+    mounted: function(){
+        dataBase.ref('tareas/').on('value', function(snapshot){
+           //console.log(snapshot.val());
+            vm.tareas=snapshot.val();
+            vm.tareas=[];
+            var objeto = snapshot.val();
+            for(var propiedad in objeto){
+                vm.tareas.unshift({
+                    '.key': propiedad,
+                    completado: objeto[propiedad].completado,
+                    titulo: objeto[propiedad].titulo
+                });
+            }
+        });
+    },
     data:{
         tema: 'Vuejs & Firebase',
-
-        tareas:[
-            {titulo:'Salir a correr.', completado:false},
-            {titulo:'Ir al gimnasio.', completado:false},
-            {titulo:'Limpiar el coche.', completado:true},
-            {titulo:'Hacer la compra.', completado:false},
-            {titulo:'Aprender Vuejs & Firebase', completado:false}
-        ]
+        tareas:[]
     },
 
 });
